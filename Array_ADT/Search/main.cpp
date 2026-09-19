0f83ce7f46bfbe1697767cd3c620dda382fe4302
farhan <mdfarhanahmadkhan7786@gmail.com>
2026-09-19 12:53:18 +0530

    wrote the binary search algorithm for unique and sorted elements arrays

--- Array_ADT/Search/main.cpp
+++ Array_ADT/Search/main.cpp
@@ -34,6 +34,24 @@ class Array{
             return -1;
         }
 
+        int binarySearch(int num){
+            int i = 0;
+            int j = length-1;
+            while(i <= j){
+                int middle = (i+j)/2;
+                if(A[middle] == num){
+                    return num;
+                }
+                else if(A[middle] < num){
+                    i = middle + 1;
+                }
+                else if(A[middle] > num){
+                    j = middle - 1;
+                }
+            }
+            return -1;
+        }
+
         void show(){
             for (int i = 0; i < length; i++)
             {
@@ -45,8 +63,8 @@ class Array{
 
 int main(){
     Array arr(10);
-    arr.initialization(5);
+    arr.initialization(10);
     arr.show();
-    cout<<arr.linearSearch(30);
+    cout<<arr.binarySearch(30);
     return 0;
 }
