b85b975899df06919fe23f47bcdffbd6ea64a3b0
farhan <mdfarhanahmadkhan7786@gmail.com>
2026-09-20 23:51:17 +0530

    wrote some algo

---
+++ Array_ADT/getSetAverageMax/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+class Array{
+    private:
+        int* A;
+        int size;
+        int length;
+
+    public:
+        Array(int size){
+            this->size = size;
+            A = new int[size];
+            length = 0;
+        }
+
+        void initialization(){
+            cout<<"Enter the elements in array";
+            length = 5;
+            for (int i = 0; i < length; i++)
+            {
+                cin>>A[i];
+                
+            }
+            
+        }
+        void show(){
+            for (int i = 0; i < length; i++)
+            {
+                cout<<A[i]<<"  ";
+            }
+            
+        }
+        int get_element(int index){
+            return A[index];
+        }
+        void set_element(int index, int num){
+            A[index] = num;
+        }
+
+
+};
+
+int main(){
+    Array arr(10);
+    arr.initialization();
+    cout << arr.get_element(3);
+    arr.set_element(3, 45);
+    arr.show();
+    return 0;
+}
